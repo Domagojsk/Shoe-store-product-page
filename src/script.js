d5667a336a67f973de2f0a5875ae099507b2a34e
domagojsk <domagoj.bane@gmail.com>
2022-05-18 19:25:27 +0200

    initial commit

--- src/script.js
+++ src/script.js
@@ -4,12 +4,128 @@
 const openMenu = document.querySelector('.hamburger');
 const closeMenu = document.querySelector('.close-menu');
 const mobileNav = document.querySelector('.main-nav');
+
 // GALLERY SECTION
 const imageGallery = document.querySelectorAll('.thumb');
 const mainImg = document.querySelector('.main-img');
 const nextImg = document.querySelector('.next');
 const prevImg = document.querySelector('.prev');
 
+// CART
+const cart = document.querySelector('.cart');
+const emptyMsg = document.querySelector('.empty-msg');
+const cartCounter = document.querySelector('.cart-counter');
+const productContainer = document.querySelector('.product-container');
+const cartContainer = document.querySelector('.cart-container');
+const price = document.querySelector('.price');
+
+const counter = document.querySelector('.counter');
+const cartBtn = document.querySelector('.btn-cart');
+
+const btnMinus = document.querySelector('.btn-minus');
+const btnPlus = document.querySelector('.btn-plus');
+const orderBtn = document.querySelector('.order-btn');
+const cartCheckoutBtn = document.querySelector('.checkout-btn');
+// CART
+btnPlus.addEventListener('click', increase);
+btnMinus.addEventListener('click', decrease);
+
+cartBtn.addEventListener('click', openCart);
+
+orderBtn.addEventListener('click', addQtyToCart);
+
+function increase() {
+  let value = parseInt(counter.innerHTML);
+  value = value + 1;
+  counter.innerHTML = value;
+}
+
+function decrease() {
+  let value = parseInt(counter.innerHTML);
+  if (value > 0) {
+    value = value - 1;
+    counter.innerHTML = value;
+  }
+}
+
+// OPEN CART
+function openCart() {
+  if (cartContainer.classList.contains('hidden')) {
+    cartContainer.classList.remove('hidden');
+  } else {
+    cartContainer.classList.add('hidden');
+  }
+}
+// CART QTY
+let qty = 0;
+function addQtyToCart() {
+  qty = qty + parseInt(counter.innerHTML);
+
+  let productHtml = `<div class="product-container">
+  <div class="cart-item">
+<img
+  class="sneakers-img"
+  src="/images/image-product-1-thumbnail.jpg"
+  alt="Sneakers image in cart"
+/>
+<div class="product-details">
+  <div class="product-name">Autumn Limited Edition...</div>
+  <div class="price-container">
+    <div class="price">$125.00</div>
+    <span class="price-x">x</span>
+    <div class="count">${qty}</div>
+    <div class="total"> $${125.0 * qty}</div>
+  </div>
+</div>
+<img
+  class="delete-btn"
+  src="/images/icon-delete.svg"
+  alt="delete button"
+/>
+</div>
+</div>`;
+  productContainer.innerHTML = productHtml;
+  cartCounter.innerText = qty;
+  // counter.innerText = 0;
+
+  const deleteBtn = document.querySelector('.delete-btn');
+  deleteBtn.addEventListener('click', decreaseQtyInCart);
+
+  if (
+    qty > 0 &&
+    cartCounter.classList.contains('hidden') &&
+    cartCheckoutBtn.classList.contains('hidden')
+  ) {
+    cartCounter.classList.remove('hidden');
+    cartCheckoutBtn.classList.remove('hidden');
+    emptyMsg.style.display = 'none';
+  } else {
+    cartCounter.classList.add('hidden');
+    cartCheckoutBtn.classList.add('hidden');
+    emptyMsg.style.display = 'block';
+  }
+}
+
+function decreaseQtyInCart() {
+  const count = document.querySelector('.count');
+  const total = document.querySelector('.total');
+
+  cartCounter.innerHTML--;
+  // total.innerHTML = total.innerHTML - price.innerHTML;
+
+  count.innerHTML = cartCounter.innerHTML;
+
+  if (cartCounter.innerHTML && count.innerHTML == 0) {
+    productContainer.innerHTML = '';
+    cartCheckoutBtn.classList.add('hidden');
+    emptyMsg.style.display = 'block';
+  } else if (cartCounter.innerHTML && count.innerHTML > 0) {
+    cartCheckoutBtn.classList.remove('hidden');
+    emptyMsg.style.display = 'none';
+  }
+}
+
+// HAM MENU
 openMenu.addEventListener('click', () => {
   mobileNav.classList.toggle('hidden');
 });
